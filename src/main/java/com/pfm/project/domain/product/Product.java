8eb18a36fb3d3a79aaed35b432a47357e34f411d
hwang seung joon <asherolleh@gmail.com>
2023-05-10 22:59:24 +0900

    <FEAT> add product and store entity

--- src/main/java/com/pfm/project/domain/product/Product.java
+++ src/main/java/com/pfm/project/domain/product/Product.java
@@ -1,2 +1,40 @@
-package com.pfm.project.domain.product;public class Product {
+package com.pfm.project.domain.product;
+
+import com.pfm.project.domain.store.Store;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "PRODUCT")
+public class Product {
+    // 상품 아이디
+    @Id
+    @GeneratedValue
+    @Column(name = "product_id", nullable = false)
+    private int id;
+
+    // 상품명
+    @Column(name="product_name", nullable = false)
+    private String productName;
+
+    // 상품 가격
+    @Column(name="price", nullable = false)
+    private int price;
+
+    @ManyToOne(targetEntity = Store.class, fetch = FetchType.LAZY)
+    @JoinColumn(name="store_id")
+    private Store store;
+
+    @Builder
+    public Product(Store store, String productName, int price) {
+        this.store = store;
+        this.price = price;
+        this.productName =productName;
+    }
 }
--- src/main/java/com/pfm/project/domain/store/Store.java
+++ src/main/java/com/pfm/project/domain/store/Store.java
@@ -1,2 +1,78 @@
-package com.pfm.project.domain.store;public class Store {
+package com.pfm.project.domain.store;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name="STORE")
+public class Store {
+
+    // 업소 아이디
+    @Id
+    @Column(name="store_id")
+    private int storeId;
+
+    // 업소명
+    @Column(name = "store_name", nullable = false)
+    private String storeName;
+
+    // 분류 코드
+    @Column(name = "store_type", nullable = false)
+    private int storeType;
+
+    // 업소 전화 번호
+    @Column(name = "store_number", nullable = false)
+    private String storeNumber;
+
+    // 찾아 오시는 길
+    @Column(name = "store_waytocome")
+    private String storeWayToCome;
+
+    // 자랑거리
+    @Column(name = "store_pride")
+    private String storePride;
+
+    // 업소 사진
+    @Column(name = "store_url")
+    private String storeUrl;
+
+    // 업소 정보
+    @Column(name = "store_info")
+    private String storeInfo;
+
+    // 업소 주소
+    @Column(name = "store_address", nullable = false)
+    private String storeAddress;
+
+    @Builder
+    public Store(
+            String storeName,
+            int storeType,
+            String storeNumber,
+            String storeWayToCome,
+            String storePride,
+            String storeUrl,
+            String storeInfo,
+            String storeAddress
+    ) {
+        this.storeName = storeName;
+        this.storeType = storeType;
+        this.storeNumber = storeNumber;
+        this.storeWayToCome = storeWayToCome;
+        this.storePride = storePride;
+        this.storeUrl = storeUrl;
+        this.storeInfo = storeInfo;
+        this.storeAddress = storeAddress;
+    }
 }
