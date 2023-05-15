1db4150635acb5c811e7572dde86ac65d64ab457
hwang seung joon <asherolleh@gmail.com>
2023-05-16 02:24:25 +0900

    <FEAT> add success response body and error response body with class
    Signed-off-by: hwang seung joon <asherolleh@gmail.com>

--- src/main/java/com/pfm/project/dto/SuccessResponseBody.java
+++ src/main/java/com/pfm/project/dto/SuccessResponseBody.java
@@ -3,15 +3,39 @@ package com.pfm.project.dto;
 import lombok.Builder;
 import org.springframework.http.HttpStatus;
 
-public class SuccessResponseBody {
+public class SuccessResponseBody<T> {
     private HttpStatus status = HttpStatus.OK;
     private String message = "Success";
-    private Object data  = null;
+    private T data  = null;
 
     @Builder
-    public SuccessResponseBody(HttpStatus status, String message, Object data) {
+    public SuccessResponseBody(HttpStatus status, String message, T data) {
         this.status = status;
         this.message = message;
+        this.data = data;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setData(T data) {
         this.data = data;
     }
 }
