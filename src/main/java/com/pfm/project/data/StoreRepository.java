1181b7bf8885d441b1e1a2e6fa510ee0144c1e95
hwang seung joon <asherolleh@gmail.com>
2023-05-21 18:02:57 +0900

    <FEAT> add service to found search store by map

--- src/main/java/com/pfm/project/data/StoreRepository.java
+++ src/main/java/com/pfm/project/data/StoreRepository.java
@@ -1,15 +1,83 @@
 package com.pfm.project.data;
 
 import com.pfm.project.domain.store.Store;
-import com.pfm.project.dto.store.response.StoreDetail;
-import com.pfm.project.dto.store.response.StoreDetailResponse;
-import com.sun.xml.bind.v2.model.core.ID;
+import com.pfm.project.dto.store.response.StoreBriefInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
     @Query("select s from Store as s join fetch s.place join fetch  s.product where s.storeId = :id")
     Optional<Store> findById(Long id);
+
+    @Query(value = "select " +
+                "s.store_id as storeId, s.store_name as storeName, s.store_type as storeType, s.store_pride as storePride, s.store_address as storeAddress, p.latitude as latitude, p.longitude as longitude, " +
+            "ST_DISTANCE_SPHERE(POINT(:userLatitude, :userLongitude), Point(p.latitude, p.longitude)) as dist " +
+            "from " +
+                "pfm_local.store as s " +
+            "inner join pfm_local.place as p on s.store_id = p.store_id " +
+            "where " +
+                "p.latitude <= :upLeftLatitude " +
+                "and p.latitude >= :downRightLatitude " +
+                "and p.longitude >= :upLeftLongitude " +
+                "and p.longitude <= :downRightLongitude " +
+            "group by s.store_id order by dist limit 20 offset :offset", nativeQuery = true
+    )
+    List<StoreBriefInfo> findAllByMapOrderByDistance(
+            double upLeftLatitude,
+            double upLeftLongitude,
+            double downRightLatitude,
+            double downRightLongitude,
+            double userLatitude,
+            double userLongitude,
+            int offset
+        );
+
+//    @Query(value = "select s.store_id as storeId, s.store_name as storeName, s.store_type as storeType, s.store_pride as storePride, s.store_address as storeAddress, pl.latitude as latitude, pl.longitude as longitude, " +
+//            "ST_DISTANCE_SPHERE(POINT(:userLatitude, :userLongitude), Point(pl.latitude, pl.longitude)) AS dist " +
+//            "from pfm_local.store as s inner join pfm_local.place as pl on s.store_id = pl.store_id " +
+//            "left join pfm_local.product as pr on s.store_id = pr.store_id " +
+//            "where pl.latitude between :downRightLatitude and :upLeftLatitude and pl.longitude between :upLeftLongitude and :downRightLongitude " +
+//            "and (s.store_name like concat('%', :userInput, '%') or pr.product_name like concat('%', :userInput, '%')) " +
+//            "group by s.store_id order by " +
+//            "case when s.store_name like concat('%', :userInput, '%') then 1 " +
+//            "when pr.product_name like concat('%', :userInput, '%') then 2 else 3 end, dist asc", nativeQuery = true)
+//    List<StoreBriefInfo> findAllByMapOrderByDistanceWithWord(
+//            double upLeftLatitude,
+//            double upLeftLongitude,
+//            double downRightLatitude,
+//            double downRightLongitude,
+//            double userLatitude,
+//            double userLongitude,
+//            String userInput,
+//            int offset
+//    );
+
+//    @Query(value = "select distinct " +
+//            "s.store_id as storeId, s.store_name as storeName, s.store_type as storeType, s.store_pride as storePride, s.store_address as storeAddress, pl.latitude as latitude, pl.longitude as longitude, " +
+//            "ST_DISTANCE_SPHERE(POINT(:userLatitude, :userLongitude), Point(pl.latitude, pl.longitude)) as dist " +
+//            "from " +
+//            "pfm_local.store as s " +
+//            "inner join pfm_local.place as pl on s.store_id = pl.store_id " +
+//            "inner join pfm_local.product as pr on s.store_id = pr.store_id " +
+//            "where " +
+//            "pl.latitude <= :upLeftLatitude " +
+//            "and pl.latitude >= :downRightLatitude " +
+//            "and pl.longitude >= :upLeftLongitude " +
+//            "and pl.longitude <= :downRightLongitude " +
+//            "and (s.store_name like concat('%', :userInput, '%') or pr.product_name like concat('%', :userInput, '%')) " +
+//            "group by s.store_id " +
+//            "order by " +
+//            "case when s.store_name like concat('%', :userInput, '%') then 1 " +
+//            "when pr.product_name like concat('%', :userInput, '%') then 2 " +
+//            "else 3 " +
+//            "end, dist asc", nativeQuery = true
+//    )
+//    "order by case " +
+//            "when s.store_name like CONTACT('%', :userInput, '%') then 1 " +
+//            "when pr.product_name like CONTACT('%', :userInput, '%') then 2 " +
+//            "else 3 " +
+//            "end, distance asc", nativeQuery = true
 }
--- src/main/java/com/pfm/project/service/SearchService.java
+++ src/main/java/com/pfm/project/service/SearchService.java
@@ -1,9 +1,89 @@
 package com.pfm.project.service;
 
+import com.pfm.project.data.StoreRepository;
+import com.pfm.project.domain.store.Store;
+import com.pfm.project.dto.place.response.PlaceResponse;
+import com.pfm.project.dto.store.request.SearchStoreByMapReqeust;
+import com.pfm.project.dto.store.response.StoreBriefInfo;
+import com.pfm.project.dto.store.response.StoreBriefInfoResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SearchService {
+
+    private final StoreRepository storeRepository;
+
+    private int getOffsetByPage(int page) {
+        return (page) * 20;
+    }
+
+
+    @Autowired
+    public SearchService(StoreRepository storeRepository) {
+        this.storeRepository = storeRepository;
+    }
+
+    public List<StoreBriefInfoResponse> searchStoreByMap(SearchStoreByMapReqeust searchStoreByMapReqeust) {
+        String userInput = searchStoreByMapReqeust.getStoreName();
+        Integer storeType = searchStoreByMapReqeust.getStoreType();
+        List<StoreBriefInfo> sqlResults = null;
+        List<StoreBriefInfoResponse> result = new ArrayList<>();
+
+        if (userInput != null) {
+//            sqlResults = storeRepository.findAllByMapOrderByDistanceWithWord(
+//                    searchStoreByMapReqeust.getLeftUpPlace().getLatitude(),
+//                    searchStoreByMapReqeust.getLeftUpPlace().getLongitude(),
+//                    searchStoreByMapReqeust.getRightDownPlace().getLatitude(),
+//                    searchStoreByMapReqeust.getRightDownPlace().getLongitude(),
+//                    searchStoreByMapReqeust.getUserPlace().getLatitude(),
+//                    searchStoreByMapReqeust.getUserPlace().getLongitude(),
+//                    searchStoreByMapReqeust.getStoreName(),
+//                    getOffsetByPage(searchStoreByMapReqeust.getPage())
+//            );
+
+        } else if (storeType != null) {
+
+
+        } else {
+            sqlResults = storeRepository.findAllByMapOrderByDistance(
+                    searchStoreByMapReqeust.getLeftUpPlace().getLatitude(),
+                    searchStoreByMapReqeust.getLeftUpPlace().getLongitude(),
+                    searchStoreByMapReqeust.getRightDownPlace().getLatitude(),
+                    searchStoreByMapReqeust.getRightDownPlace().getLongitude(),
+                    searchStoreByMapReqeust.getUserPlace().getLatitude(),
+                    searchStoreByMapReqeust.getUserPlace().getLongitude(),
+                    getOffsetByPage(searchStoreByMapReqeust.getPage())
+            );
+//            System.out.println(store[0]);
+        }
+        for (StoreBriefInfo s : sqlResults) {
+            result.add(
+                    StoreBriefInfoResponse.builder()
+                            .storeId(s.getStoreId())
+                            .storeName(s.getStoreName())
+                            .storeType(s.getStoreType())
+                            .storePride(s.getStorePride())
+                            .storeAddress(s.getStoreAddress())
+                            .place(
+                                    PlaceResponse.builder()
+                                            .latitude(s.getLatitude())
+                                            .longitude(s.getLongitude())
+                                            .build()
+                            )
+                            .build()
+            );
+        }
+
+
+
+        return result;
+
+    }
 
 
 //    public List<SearchResponse> currentLocation(String search, HomeCoordinatesRequest req){
